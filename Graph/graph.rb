160ab1bd5b371dea644fa07b2ea0ee204a16732c
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-04-03 10:17:10 -0700

    graphs

--- Graph/graph.rb
+++ Graph/graph.rb
@@ -1,27 +1,17 @@
-def graph(hash_graph)
-  # write your code here
-  path = []
-
-  # hash.each do |key, value|
-  # path << value
-
-  # hash.each do |key|
-  # hash[key].each { |val| path << val[0] }
-
-  i = 0
-  until hash_graph.nil?
-    puts number = hash_graph.values[0].to_i
-    puts nextNum = hash_graph.values[number]
-    return path
-  end
+def graph(hash_graph, key = 0, path=[])
+	path << key if path.empty?
+	val = hash_graph[key][0]
+	path << val
+	graph(hash_graph, key = val, path) unless val == 4
+	return path
 end
 
-hash = {
-  0 => [2],
-  1 => [4],
-  2 => [5, 0, 3],
-  3 => [2],
-  4 => [1, 5],
+hash = { 
+  0 => [2], 
+  1 => [4], 
+  2 => [5, 0, 3], 
+  3 => [2], 
+  4 => [1, 5], 
   5 => [4, 2]
 }
 
