b108eaf6ff1345e5a960b99c00205ad64a043efb
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-04-29 10:29:36 -0700

    Adding model solution for bfs

--- firstBreadthsearch/firstBreadth.rb
+++ firstBreadthsearch/firstBreadth.rb
@@ -25,3 +25,28 @@ p bfs({
   5 => [4, 2]
 })
 # => [0, 2, 5, 3, 4, 1]
+
+=begin
+Model solution
+
+def bfs(graph)
+  # write your code here
+  queue = [0]
+  result = []
+  visited = []
+  
+  until queue.empty?
+    head = queue.shift
+    result << head
+    visited << head
+    
+    not_visited = graph[head].reject { |node| visited.include? node }
+    
+    visited += not_visited
+    queue += not_visited
+  end
+  
+  result
+end
+
+=end
