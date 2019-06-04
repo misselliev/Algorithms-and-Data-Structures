fd03f3ecaf6bfe4271fe51c4c1b341c887da768c
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-06-04 11:46:50 -0700

    Adding model soltion for quicksort

--- Sorting algorithms/quicksort1.rb
+++ Sorting algorithms/quicksort1.rb
@@ -2,13 +2,31 @@
 
 def partition(array)
   model = array[0]
-  right =[]
+  right = []
   left = []
-  for i in 1..array.length - 1 do 
-  array[i] > model ? right << array[i] : left << array[i]
+  (1..array.length - 1).each do |i|
+    array[i] > model ? right << array[i] : left << array[i]
+  end
+  right.unshift(model).unshift(left).flatten!
 end
-right.unshift(model).unshift(left).flatten!
-end
+
+# MODEL SOLUTION
+# def partition(array)
+#   # write your code here
+#   pivot = array.first
+#   smaller = []
+#   greater = []
+
+#   array[1..-1].each do |number|
+#     if number > pivot
+#       greater << number
+#     else
+#       smaller << number
+#     end
+#   end
+
+#   smaller + [pivot] + greater
+# end
 
 p partition([4, 5, 3, 9, 1])
 # => [3, 1, 4, 5, 9]
