b469eaddf975fd571e103189072c31a9cff3f1de
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-06-19 12:10:53 -0700

    2nd attempt at simplequicksort

---
+++ Sorting algorithms/simplesort_2.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+def simple_quicksort(array)
+  return [] if array.empty?
+
+  pivot = array.first
+  right = []
+  left = []
+  (1..array.length - 1).each do |i|
+    array[i] > pivot ? right << array[i] : left << array[i]
+  end
+  # array.unshift(pivot)
+  p left unless left.empty?
+  p right unless right.empty?
+
+  [simple_quicksort(left), pivot, simple_quicksort(right)].flatten
+end
+
+p simple_quicksort([5, 8, 1, 3, 7, 10, 2])
+# => 2 3
+#    1 2 3
+#    7 8 10
+#    1 2 3 5 7 8 10
