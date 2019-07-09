5ed1644dbeb682dda0d3c1bb964121a6db707503
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-07-09 15:50:58 -0700

    Merge sort challenge

---
+++ Sorting challenges/mergesort.rb
@@ -0,0 +1,26 @@
+def merge_sort(array1, array2)
+  result = []
+  i, j = 0,0
+  while i < array1.length && j < array2.length do
+    if array2[j] > array1[i]
+      result.push(array1[i])
+      i+=1
+    else
+      result.push(array2[j])
+      j+=1
+    end
+  end
+    while i < array1.length do
+    result.push(array1[i])
+    i+=1;
+  end
+  while j < array2.length do
+    result.push(array2[j])
+        j+=1;
+  end
+  return result
+end
+
+
+p merge_sort([1, 3, 9, 11], [2, 4, 6, 8])
+# => [1, 2, 3, 4, 6, 8, 9, 11]
