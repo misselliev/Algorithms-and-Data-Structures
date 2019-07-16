009143b0f0f7e744fcd062e7836255ae9385f643
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-07-16 12:28:07 -0700

    Median

---
+++ Sorting challenges/timescheduler.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+def time_scheduler(array)
+
+end
+
+# def split(array)
+#   start = []
+#   finish = []
+#   array.each_with_index do |num, idx|
+#     idx.even? ? start.push(num) : finish.push(num)
+#   end
+# end
+
+p time_scheduler([4, 8, 1, 3, 7, 9, 5, 6])
+# => [[1,3], [5,6], [7,9]]
