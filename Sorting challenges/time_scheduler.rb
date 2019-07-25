94e478cc5e4be20dce6324ffc0712066b5a0e3a7
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-07-25 12:51:07 -0700

    Scheduler greedy algorithm

--- Sorting challenges/time_scheduler.rb
+++ Sorting challenges/time_scheduler.rb
@@ -7,16 +7,18 @@ def time_scheduler(array)
   sorted = array.each_slice(2).sort_by { |_x, y| y }
   # adding first element that takes least amount of time to complete
   answer.push(sorted[0])
-
+  size = sorted.length 
+  
+  # comparing finish time with start time
   until a == sorted.length - 2
-    # p sorted.length => 4
-    answer.push(sorted[a + 1]) if sorted[a][1] < sorted[a + 1][0]
+    answer.push(sorted[a + 1]) if sorted[a][1] <= sorted[a + 1][0] || answer.last[1] < sorted[a+1][0]
     a += 1
   end
-  # pushing last item if it complies with condition
-  answer.push(sorted[sorted.length - 1]) if sorted[sorted.length - 2][1] < sorted[sorted.length - 1][0]
+  
+  # pushing last item if it complies
+  answer.push(sorted[size - 1]) if sorted[size - 2][1] >= sorted[size - 1][0]
 
-  p answer
+  answer
 end
 
 p time_scheduler([4, 8, 1, 3, 7, 9, 5, 6])
